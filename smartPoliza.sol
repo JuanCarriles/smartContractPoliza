f9ca2923bfa3ca69d08511f064b6da7d227633fa
JuanCarriles <91573845+JuanCarriles@users.noreply.github.com>
2022-10-14 01:07:12 -0300

    Update smartPoliza.sol

--- smartPoliza.sol
+++ smartPoliza.sol
@@ -1,70 +1,22 @@
 pragma solidity ^0.5.0;
 
-contract PolizaCar {
- 
-   uint public numeroPoliza;
-   string public fechaContratacion; 
-   string public fechaVencimiento;
-   string public tipoDocumento;
-   string public documentCliente;
-   string public nombreCliente;
-   string public matriculaVehiculo;
+contract SimpleStorage {
 
-   event nuevaPoliza(uint numeroPoliza, string fechaContratacion, string fechaVencimiento, string tipoDocumento, string documentCliente, string nombreCliente, uint matriculaVehiculo);
+    event ValueChanged(address indexed author, string oldValue, string newValue);
 
-    struct Poliza{
-      uint numeroPoliza;
-      string fechaContratacion;
-      string fechaVencimiento;
-      string tipoDocumento;
-      string documentCliente;
-      string nombreCliente;
-      string matriculaVehiculo;
-    }
-    
-    constructor(uint numPol, string memory fechaCon, string memory fechaVen, string memory tipoDoc, string memory documentCli, string memory nombreCli, string memory matriculaVehi) public {
-      numeroPoliza = numPol;
-      fechaContratacion  = fechaCon;
-      fechaVencimiento = fechaVen;
-      tipoDocumento = tipoDoc;
-      documentCliente = documentCli;
-      nombreCliente = nombreCli;
-      matriculaVehiculo = matriculaVehi;
-   }
-    
+    string storedValue;
 
-    function set(uint num, string memory fechaC, string memory fechaV, string memory   tipoD, string  memory documentC, string memory nombreC, uint matriculaV) public {
-    
-      emit nuevaPoliza(num, fechaC, fechaV, tipoD, documentC, nombreC, matriculaV);
-      
+    constructor(string memory newValue) public {
+        emit ValueChanged(msg.sender, storedValue, newValue);
+        storedValue = newValue;
     }
 
-    function getNumeroPoliza() public view returns (uint retNum) {
-      return numeroPoliza;
-   }
-    function getFechaContratacion() public view returns (string memory retFechC) {
-       return fechaContratacion;
-    }
-    
-    function getFechaVencimiento() public view returns (string memory retFechV) {
-       return fechaVencimiento;
-    }
-    
-    function getTipoDocumento() public view returns (string memory retTipoDoc) {
-       return tipoDocumento;
+    function getValue() public view returns (string memory value) {
+        return storedValue;
     }
 
-    function getDocumentoCliente() public view returns (string memory retNom) {
-      return documentCliente;
-    } 
-    
-    function getNombre() public view returns (string memory retNombre) {
-       return nombreCliente;
+    function setValue(string memory newValue) public {
+        emit ValueChanged(msg.sender, storedValue, newValue);
+        storedValue = newValue;
     }
-
-    function getMatricula() public view returns (string memory retMat) {
-       return matriculaVehiculo;
-    }
-
-
 }
